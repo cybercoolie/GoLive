9af8791ae21d605f988ccebdab10f583f7b104d0
ramverma238 <ramverma238@hotmail.com>
2024-03-22 20:17:27 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoLive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
